2a48c0c6a0890e5482a34a36cfecd0c2c3a6728f
Francesco Crimi <francrim@gmail.com>
2021-07-25 17:52:55 +0200

    clean up

--- FormApp/Program.cs
+++ FormApp/Program.cs
@@ -3,17 +3,14 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace FormApp
 {
     public static class Program
     {
         [STAThread]
-        public static async Task Main(string[] args)
-        {
+        public static async Task Main(string[] args) =>
             await CreateHostBuilder(args).Build().RunAsync();
-        }
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
